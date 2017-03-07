64439436fddb8a8c65b0bfaddb15c0dd43e633be
aslanovr <aslanov.r.m@gmail.com>
2017-03-07 17:42:19 +0300

    Initial commit

---
+++ src/Arena.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Ruslan on 07.03.2017.
+ */
+public class Arena {
+    Fighter fighterArni = new Fighter("Arnold", 100, 500);
+    Fighter fighterEdy = new Fighter("Edy", 100, 459);
+}
---
+++ src/Fighter.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ruslan on 07.03.2017.
+ */
+public class Fighter {
+    String name;
+    int health;
+    int strength;
+    protected Fighter(String name, int health, int strength){
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+}
