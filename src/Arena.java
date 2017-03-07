96315c6b573e83f7e4735219aab9ce5e0cdb3840
aslanovr <aslanov.r.m@gmail.com>
2017-03-07 21:35:18 +0300

    Убрал инициализацию бойцов

--- src/Arena.java
+++ src/Arena.java
@@ -2,6 +2,6 @@
  * Created by Ruslan on 07.03.2017.
  */
 public class Arena {
-    Fighter fighterArni = new Fighter("Arnold", 100, 500);
-    Fighter fighterEdy = new Fighter("Edy", 100, 459);
+    Fighter fighterArni;
+    Fighter fighterEdy;
 }
